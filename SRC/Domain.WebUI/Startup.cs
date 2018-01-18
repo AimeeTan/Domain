19785043d6bf095651d1dd8e4c4e2acb06bf51d7
Aimee tan <shenglan_tan@163.com>
2018-01-19 00:17:03 +0800

    .

--- SRC/Domain.WebUI/Startup.cs
+++ SRC/Domain.WebUI/Startup.cs
@@ -65,12 +65,12 @@ namespace Domain.WebUI
 
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
         }
     }
 }
