b8b6e5c3b63e80ea39c51f71304ca6056fc535c5
Aimee tan <shenglan_tan@163.com>
2018-01-18 11:44:40 +0800

    UI

---
+++ SRC/Domain.WebUI/src/vendor.browser.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("@angular/platform-browser");
+require("@angular/platform-browser-dynamic");
+require("@angular/core");
+require("@angular/common");
+require("@angular/forms");
+require("@angular/http");
+require("@angular/router");
+require("@angularclass/hmr");
+// RxJS
+require("rxjs/add/operator/map");
+require("rxjs/add/operator/mergeMap");
+require("jquery");
+require("tether");
+require("bootstrap");
+require("widgster");
+require("pace");
+require("ngx-uploader");
+if ('production' === ENV) {
+    // Production
+}
+else {
+    // Development
+}
+//# sourceMappingURL=vendor.browser.js.map
