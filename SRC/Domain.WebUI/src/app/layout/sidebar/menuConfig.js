607dc62eb873af2a3f9d74dddaac3fe9fa13f86b
Aimee tan <shenglan_tan@163.com>
2018-01-18 23:35:16 +0800

    。

--- SRC/Domain.WebUI/src/app/layout/sidebar/menuConfig.js
+++ SRC/Domain.WebUI/src/app/layout/sidebar/menuConfig.js
@@ -1,17 +1,39 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.menus = [
-    //{
-    //name: 'DASHBOARD',
-    //className: 'fa fa-desktop',
-    //twoLevel: false,
-    //url: 'dashboard'
-    //},
     {
-        name: 'PARCEL',
+        name: '系统配置',
         className: 'fa fa-cube',
         twoLevel: false,
-        url: 'parcel/list',
-    }
+        url: 'system/configuration',
+    },
+    {
+        typeName: '自动采集扫描',
+        className: 'fa fa-exclamation-circle',
+        twoLevel: true,
+        node: [{
+                name: '域名扫描',
+                twoLevel: false,
+                url: 'domain/scan'
+            }, {
+                name: '历史记录',
+                twoLevel: false,
+                url: 'domain/list'
+            }]
+    },
+    {
+        typeName: '自定义扫描',
+        className: 'fa fa-exclamation-circle',
+        twoLevel: true,
+        node: [{
+                name: '上传扫描',
+                twoLevel: false,
+                url: 'ribbon/scan'
+            }, {
+                name: '历史记录',
+                twoLevel: false,
+                url: 'ribbon/list'
+            }]
+    },
 ];
 //# sourceMappingURL=menuConfig.js.map
--- SRC/Domain.WebUI/src/app/layout/sidebar/menuConfig.ts
+++ SRC/Domain.WebUI/src/app/layout/sidebar/menuConfig.ts
@@ -1,16 +1,40 @@
 ﻿import { IMenu } from '../../model/menu'
 export const menus: IMenu[] = [
-	//{
-	//name: 'DASHBOARD',
-	//className: 'fa fa-desktop',
-	//twoLevel: false,
-	//url: 'dashboard'
-	//},
 	{
-	name: 'PARCEL',
+	name: '系统配置',
 	className: 'fa fa-cube',
 	twoLevel: false,
-	url: 'parcel/list',
-}]
+	url: 'system/configuration',
+	},
+	{
+		typeName: '自动采集扫描',
+		className: 'fa fa-exclamation-circle',
+		twoLevel: true,
+		node: [{
+			name: '域名扫描',
+			twoLevel: false,
+			url: 'domain/scan'
+		}, {
+			name: '历史记录',
+			twoLevel: false,
+			url: 'domain/list'
+		}]
+	}, 
+	{
+		typeName: '自定义采集扫描',
+		className: 'fa fa-exclamation-circle',
+		twoLevel: true,
+		node: [{
+			name: '上传扫描',
+			twoLevel: false,
+			url: 'ribbon/scan'
+		}, {
+			name: '历史记录',
+			twoLevel: false,
+			url: 'ribbon/list'
+		}]
+	},
+
+]
 
 
