f7c9b359eb7d2ec3582183dde345d993216d268c
Aimee tan <shenglan_tan@163.com>
2018-01-18 22:28:24 +0800

    链接DB

---
+++ SRC/Domain.Data/Store/Domain_SysDataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.Data.Store
+{
+	partial class Domain_SysDataContext
+	{
+		//public Domain_SysDataContext() :
+		//	base(Core.Properties.Settings.Default.Domain_SysDatabaseConnectionString)
+		//{
+		//	OnCreated();
+		//}
+	}
+}
