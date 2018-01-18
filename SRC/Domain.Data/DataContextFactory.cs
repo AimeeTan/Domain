2a4d44c7ba2ef58b2f42e03d9ce568599c60d943
Aimee tan <shenglan_tan@163.com>
2018-01-18 18:38:13 +0800

    .

---
+++ SRC/Domain.Data/DataContextFactory.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Domain.Data
+{
+	using Archetypes;
+	using Store;
+	//public sealed class ZIPDataContextFactory : IDataContextFactory<ZIPDataContext>
+	//{
+	//	public ZIPDataContext Create()
+	//		=> new ZIPDataContext { ObjectTrackingEnabled = false };
+	//}
+}
