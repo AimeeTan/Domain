68f9d354b1f2be93aa37a647671c701aee01993c
Aimee tan <shenglan_tan@163.com>
2018-01-19 18:53:39 +0800

    .

--- Domain-GateWay/Functions/SearchEngine_Row.sql
+++ Domain-GateWay/Functions/SearchEngine_Row.sql
@@ -1,10 +1,10 @@
-﻿CREATE FUNCTION [svc].[SearchEngine$Row]()
-RETURNS TABLE
-WITH SCHEMABINDING, ENCRYPTION
-AS RETURN
-(
-
-	select	p.ID, p.PID, p.AID, p.Type, p.Source, p.Alias 
-	from	core.Party#Raw() p, core.Party#Type() t
-	where	p.Type=t.ZebraStore
-)
+﻿--CREATE FUNCTION [svc].[SearchEngine$Row]()
+--RETURNS TABLE
+--WITH SCHEMABINDING, ENCRYPTION
+--AS RETURN
+--(
+--	select ID from core._SearchEngine
+--	--select	p.ID, p.PID, p.AID, p.Type, p.Source, p.Alias 
+--	--from	core.Party#Raw() p, core.Party#Type() t
+--	--where	p.Type=t.ZebraStore
+--)
