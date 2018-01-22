79bd1f2ac6bf8987d0466ed1024abf2aef7e9f7e
Aimee tan <shenglan_tan@163.com>
2018-01-23 00:14:14 +0800

    .

---
+++ Domain-GateWay/Actions/ConfirmConfig.sql
@@ -0,0 +1,18 @@
+﻿/*
+	@slip    = Many[at.Tvp.Triad.Join(x.ID, x.StartKey, x.EndKey)]
+*/
+CREATE PROCEDURE [svc].[ConfirmConfig](@slip tvp)
+AS
+BEGIN
+	SET NOCOUNT    ON;
+	SET XACT_ABORT ON;
+	BEGIN TRY
+		BEGIN	TRAN;
+
+
+		COMMIT	TRAN;
+	END TRY
+	BEGIN CATCH
+		if (xact_state() = -1) ROLLBACK TRAN; throw;
+	END CATCH
+END
